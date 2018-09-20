52128c6383592ea4f1581255baf652afe536c097
Mike <matroxmike@icloud.com>
2018-09-20 12:23:39 +1000

    after hlint, hindent and colour changes to identify parts

--- Bezier.hs
+++ Bezier.hs
@@ -2,12 +2,13 @@ module Bezier where
 
 import Control.Monad (zipWithM)
 
-type Point = [Float]  -- a multi-dimensional coordinate
-type Parametric a = Float -> a  -- a value that varies over time
+type Point = [Float] -- a multi-dimensional coordinate
+
+type Parametric a = Float -> a -- a value that varies over time
 
 -- linear interpolation between two numbers, from t=0 to t=1
 line1d :: Float -> Float -> Parametric Float
-line1d a b = \t -> (1 - t)*a + t*b
+line1d a b = \t -> (1 - t) * a + t * b
 
 -- line between two points is linear interpolation on each dimension
 line :: Point -> Point -> Parametric Point
@@ -17,6 +18,7 @@ line p q = zipWithM line1d p q
 -- line between bezier of first n-1 points and bezier of last n-1 points
 bezier :: [Point] -> Parametric Point
 bezier [p] = return p
-bezier ps  = do p <- bezier (init ps)
-                q <- bezier (tail ps)
-                line p q
+bezier ps = do
+  p <- bezier (init ps)
+  q <- bezier (tail ps)
+  line p q
